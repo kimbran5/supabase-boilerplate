e14f97b5dd583258fc80644fe6954b167fa293a4
Brandon Kim <kimbran5@msu.edu>
2023-03-20 20:12:57 -0400

    Supabase test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0"
+
+const supabaseUrl = 'https://eslchzhqritmhzzcgkmh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzbGNoemhxcml0bWh6emNna21oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNTU3NzEsImV4cCI6MTk5MzczMTc3MX0.R_xZRAZ2-_j1PFu51cKmBfvW77F22PeMLZQ_Niktgl0'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+    .from('books')
+    .select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
+  }
+}
+
+getBooks();
